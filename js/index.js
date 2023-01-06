0e6b5fc184000a1e8d7df7c7d0304b760483336a
Phuc Nguyen <p30012k1@gmail.com>
2023-01-06 21:22:27 +0700

    up

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// $(function(){
+//     let img = $('#banner #banner_slide');
+//     let content = $('#banner #banner_slide #event_content');
+//     let iCount = 1;
+//     let iMax = 3;
+//     img.css('background-image', `url('imgs/NEW-${iCount}.jpg')`);
+//     content.html(`<div>SỰ KIỆN 0${iCount}</div><div>2023</div>`);
+//     setInterval(function () {
+//         if (iCount > iMax)
+//             iCount = 1;
+//         img.css('background-image', `url('imgs/NEW-${iCount}.jpg')`);
+//         content.html(`<div>SỰ KIỆN 0${iCount}</div><div>2023</div>`);
+//         iCount++;
+//     }, 2000);
+// })
